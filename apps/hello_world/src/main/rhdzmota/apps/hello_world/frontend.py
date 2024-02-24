00912ea6f7b991884f866c25ab2395efc2bb7d91
rhdzmota <contact@rhdzmota.com>
2024-02-23 18:32:03 -0600

    APPS-6 Allow an interactive definition on the hello-world implementation

--- apps/hello_world/src/main/rhdzmota/apps/hello_world/frontend.py
+++ apps/hello_world/src/main/rhdzmota/apps/hello_world/frontend.py
@@ -1,5 +1,35 @@
+import enum
+import textwrap
+
 import streamlit as st
+
+from rhdzmota.apps.hello_world.utils import Salutation
 
 
 def view():
-    st.markdown("# Hello, World!")
+    st.markdown("# Example App: hello-world")
+
+    st.markdown(
+        textwrap.dedent(
+            """
+            Example hello-world application designed by 
+            [Rodrigo H. Mota](https://rhdzmota.com).
+            """
+        )
+    )
+
+    with st.form("form-hello-world"):
+        recipient = st.text_input(label="Recipient", value="World")
+        salutation = st.selectbox(
+            label="Salutation",
+            options=[
+                salutation
+                for salutation in Salutation
+            ],
+            format_func=lambda salutation: salutation.name.title()
+        )
+        submitted = st.form_submit_button("Submit")
+    if not submitted:
+        return
+    st.markdown(f"> {salutation.value}, {recipient}!")
+
